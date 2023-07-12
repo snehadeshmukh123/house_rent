beb0d6cf36a9e5d40e43a337299e8964c06cf3ec
snehadeshmukh123 <snehadeshmukh93@gmail.com>
2023-07-12 11:14:39 +0530

    Added modified file

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 PORT_NUMBER = 8080
 
 MONGODB_PORT_NUMBER = 2707
-MODEL_FILE_PATH= r'static\models\linear_regression.pkl'
+MODEL_FILE_PATH= os.path.join(r'static\models\linear_regression.pkl')
